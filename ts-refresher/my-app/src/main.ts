e0e2ae382862f433bd81a22b29735334ab907b92
Erick Kanja <e.kanja@alustudent.com>
2026-04-14 13:44:59 +0300

    Add function to check if is adult

--- ts-refresher/my-app/src/main.ts
+++ ts-refresher/my-app/src/main.ts
@@ -2,8 +2,18 @@
 // string): string that returns a formatted string like 
 // '$49.99'. Add full type annotations.
 
-const formatPrice = (amount: number, currency: string): string => {
-  return `${currency}${amount}`
+// const formatPrice = (amount: number, currency: string): string => {
+//   return `${currency}${amount}`
+// }
+
+// console.log(formatPrice(49.99, '$'))
+
+const isAdult = (age: number) : boolean => {
+  if(age >= 18) {
+    return true
+  } else {
+    return false
+  }
 }
 
-console.log(formatPrice(49.99, '$'))
+console.log(isAdult(10))
