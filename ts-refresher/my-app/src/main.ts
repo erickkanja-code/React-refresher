5841fa1c9cc918874d4afa8c90d288a4e1a067df
Erick Kanja <e.kanja@alustudent.com>
2026-04-14 18:52:04 +0300

    Complete arrays and tuple section and comment out all tasks

--- ts-refresher/my-app/src/main.ts
+++ ts-refresher/my-app/src/main.ts
@@ -8,12 +8,42 @@
 
 // console.log(formatPrice(49.99, '$'))
 
-const isAdult = (age: number) : boolean => {
-  if(age >= 18) {
-    return true
-  } else {
-    return false
+// const isAdult = (age: number) : boolean => {
+//   if(age >= 18) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// console.log(isAdult(10))
+
+// const getFullName = (firstName: string, lastName: string) => {
+//   return `${firstName} ${lastName}`
+// }
+
+// console.log(getFullName("Erick", "Muchiri"))
+
+// const getFirstAndLast = (arr: string[]): [string, string] => {
+//   return [arr[0], arr[arr.length - 1]]
+// }
+
+// console.log(getFirstAndLast(['h', 'e', 'l', 'o']))
+
+const result = (arr: number[]): [number, number] => {
+  let max: number = 0
+  for (let num of arr) {
+    if (num > max){
+      max = num
+    }
   }
+  let min: number = arr[0]
+  for (let num of arr) {
+    if (num < min){
+      min = num
+    }
+  }
+  return [max, min]
 }
 
-console.log(isAdult(10))
+console.log(result([9876, 333, 92, 102, 10101010]))
